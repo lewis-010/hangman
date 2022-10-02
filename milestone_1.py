4bb98dccc50dee5456a4678b7625304dac3ffa3d
Lewis Cox <lcox101101@gmail.com>
2022-10-02 11:00:00 +0100

    include user input

--- milestone_1.py
+++ milestone_1.py
@@ -7,3 +7,6 @@ word_list = ["banana", "grape", "watermelon", "apple", "plum"]
 # pass list through random module
 word = random.choice(word_list)
 print(word)
+
+# get input from user (guess the fruit)
+guess = input("Guess a letter ")
