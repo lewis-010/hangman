f9b03d3d7f2f263728b0074959f799b17bc04f5d
Lewis Cox <lcox101101@gmail.com>
2022-10-04 14:59:39 +0100

    introduce __name__ guard to milestone 1

--- milestone_1.py
+++ milestone_1.py
@@ -9,8 +9,9 @@ word = random.choice(word_list)
 # print(word)
 
 # get input from user (guess the fruit)
-guess = input("Guess a letter: ")
-if guess.isalpha() and len(guess)==1:
-    print("Good guess!")
-else:
-    print("Oops! That is not a valid input.")
+if __name__=="__main__":
+    guess = input("Guess a letter: ")
+    if guess.isalpha() and len(guess)==1:
+        print("Good guess!")
+    else:
+        print("Oops! That is not a valid input.")
