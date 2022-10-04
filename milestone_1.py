f9ba9be07cc69c372ce675b0292fdce614e423b2
Lewis Cox <lcox101101@gmail.com>
2022-10-04 14:52:16 +0100

    create ask input & check guess functions

--- milestone_1.py
+++ milestone_1.py
@@ -6,10 +6,10 @@ word_list = ["banana", "grape", "watermelon", "apple", "plum"]
 
 # pass list through random module
 word = random.choice(word_list)
-print(word)
+# print(word)
 
 # get input from user (guess the fruit)
-guess = input("Guess a letter ")
+guess = input("Guess a letter: ")
 if guess.isalpha() and len(guess)==1:
     print("Good guess!")
 else:
--- milestone_2.py
+++ milestone_2.py
@@ -1,18 +1,22 @@
-# iteratively checks user input validity
-while True:
-    guess = input("Guess a letter ")
-    if guess.isalpha() and len(guess)==1:
-        print("Good guess!")
-        break
-    else:
-        print("Invalid letter, please enter a single alphabetical character")
+# import milestone_1 code 
+import milestone_1 as ms1
 
-# checks if user input is in the word
-word = "apple"
-while True:
-    guess = str(input("Guess a letter "))
-    if guess in word:
+# check_guess function checks if guessed letter is in the word
+def check_guess (guess):
+    guess = guess.lower()
+    if guess in ms1.word:
         print(f"Good guess! {guess} is in the word.")
-        break
     else:
-        print(f"Sorry, {guess} is not in the word. Try again.")  
+        print(f"Sorry, {guess} is not in the word. Try again.")
+
+# ask_for_input function gets user to guess and checks the validity
+def ask_for_input():
+    while True:
+        guess = input("Guess a letter: ")
+        if guess.isalpha() and len(guess)==1:
+            break
+        else:
+            print("Invalid input, please enter a single alphabetical character.")
+    check_guess(guess)
+
+ask_for_input()
