a8081db6144b993dd800b88d7237a55f00aeb9c9
Lewis Cox <lcox101101@gmail.com>
2022-10-06 12:41:55 +0100

    start check_guess method definition

--- milestone_3.py
+++ milestone_3.py
@@ -9,3 +9,10 @@ class Hangman:
         self.num_lives = int(num_lives)
         self.word_list = word_list
         self.list_of_guesses = []
+
+    def check_guess(self, guess):
+        guess = guess.lower()
+        if guess in self.word:
+            print(f"Good guess! {guess} is in the word.")
+    
+    
