bcdb1334abee41e8591e15f908640a2d352adaf4
Lewis Cox <lcox101101@gmail.com>
2022-10-06 13:08:52 +0100

    create & test ask_for_input method

--- milestone_3.py
+++ milestone_3.py
@@ -1,11 +1,10 @@
 import random
 
-# create Hangman class
-class Hangman:
-    def __init__(self, word_list, num_lives=5):
+class Hangman():
+    def __init__(self, word_list=["banana", "grape", "watermelon", "apple", "plum"], num_lives=5):
         self.word = random.choice(word_list).lower()
         self.word_guessed = len(self.word)*["_"]
-        self.num_letters = int(set(self.word))
+        self.num_letters = len(set(self.word))
         self.num_lives = int(num_lives)
         self.word_list = word_list
         self.list_of_guesses = []
@@ -14,5 +13,17 @@ class Hangman:
         guess = guess.lower()
         if guess in self.word:
             print(f"Good guess! {guess} is in the word.")
+            self.list_of_guesses.append(guess) 
     
+    def ask_for_input(self):
+        while True:
+            guess = input("Guess a letter: ")
+            if len(guess)>1 or not guess.isalpha():
+                print("Invald letter. Please enter a single alphabetical character.")
+            elif guess in self.list_of_guesses:
+                print("You already tried that letter.")
+            else:
+                self.check_guess(guess)
+                break
     
+Hangman().ask_for_input()
